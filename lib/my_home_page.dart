5ea5f1a483bd766b40d78bfe9e46bb92dc0dc867
Артём Лобазин <artem@yaport.local>
2020-03-27 20:43:00 +0300

    Initial commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String url = 'https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0';
+  List data;
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+   setState(() {
+      data = json.decode(response.body);
+      //data = extractdata[];
+      //print(data);
+    });
+  }
+
+  @override
+  void initState() {
+    this.makeRequest();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Photo List'),
+        ),
+        body: new ListView.builder(
+            itemCount: data == null ? 0 : data.length,
+            itemBuilder: (BuildContext context, i) {
+              return new ListTile(
+                title: new Text(data[i]['user']["name"]),
+                subtitle: new Text(data[i]["alt_description"]),
+                leading: new CircleAvatar(
+                  backgroundImage:
+                  new NetworkImage(data[i]["urls"]['small']),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                          new SecondPage(data[i])));
+                },
+              );
+            }
+        )
+    );
+  }
+}
+/*
+class SecondPage extends StatelessWidget {
+  SecondPage(this.data);
+  final data;
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+      appBar: new AppBar(title: new Text('Second Page')),
+      body: new Center(
+        child: new Container(
+          width: 350.0,
+          height: 350.0,
+          decoration: new BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: new DecorationImage(
+              image: new NetworkImage(data["urls"]["full"]),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
+            border: new Border.all(
+              color: Colors.red,
+              width: 4.0,
+            ),
+          ),
+        ),
+      ));
+}
+
+*/
+class SecondPage extends StatelessWidget {
+  SecondPage(this.data);
+  final data;
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+      appBar: new AppBar(title: new Text('Big photo')),
+      body: new Center(
+        child: new Container(
+          child: Image.network(data["urls"]["full"]),
+
+          ),
+        ),
+      );
+}

ca468e9eec410c1dc0d636c4ead9530e2a56cedd
iswift-ru <58907233+iswift-ru@users.noreply.github.com>
2020-03-27 20:59:54 +0300

    Update my_home_page.dart

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -31,8 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
    setState(() {
       data = json.decode(response.body);
-      //data = extractdata[];
-      //print(data);
+      
     });
   }
 
@@ -70,34 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-/*
-class SecondPage extends StatelessWidget {
-  SecondPage(this.data);
-  final data;
-  @override
-  Widget build(BuildContext context) => new Scaffold(
-      appBar: new AppBar(title: new Text('Second Page')),
-      body: new Center(
-        child: new Container(
-          width: 350.0,
-          height: 350.0,
-          decoration: new BoxDecoration(
-            color: const Color(0xff7c94b6),
-            image: new DecorationImage(
-              image: new NetworkImage(data["urls"]["full"]),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
-            border: new Border.all(
-              color: Colors.red,
-              width: 4.0,
-            ),
-          ),
-        ),
-      ));
-}
 
-*/
 class SecondPage extends StatelessWidget {
   SecondPage(this.data);
   final data;
